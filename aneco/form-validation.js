cd6dbdb648d861a6fa5c5173713123fc9aa1b44f
quyendn <quyendn84@gmail.com>
2019-03-01 12:25:10 +0700

    quyendn

---
+++ aneco/form-validation.js
@@ -0,0 +1,138 @@
+﻿// Form-Validation.js
+// ====================================================================
+// This file should not be included in your project.
+// This is just a sample how to initialize plugins or components.
+//
+// - ThemeOn.net -
+
+$(document).ready(function() {
+
+
+    // FORM VALIDATION
+    // =================================================================
+    // Require Bootstrap Validator
+    // http://bootstrapvalidator.com/
+    // =================================================================
+    // FORM VALIDATION FEEDBACK ICONS
+    // =================================================================
+    var faIcon = {
+            valid: 'fa fa-check-circle fa-lg text-success',
+            invalid: 'fa fa-times-circle fa-lg',
+            validating: 'fa fa-refresh'
+        }
+        // FORM VALIDATION ON ACCORDION
+        // =================================================================
+        // FORM VALIDATION CUSTOM ERROR CONTAINER
+        // =================================================================
+        // Indicate where the error messages are shown.
+        // Tooltip, Popover, Custom Container.
+        // =================================================================
+
+    $('#frmDattiec').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Địa chỉ email không được để trống.'
+                    },
+                    emailAddress: {
+                        message: 'Không đúng định dạng email'
+                    }
+                }
+            },
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 11,
+                        message: 'Số điện thoại chỉ có thể là 10 hoặc 11 số.'
+                    }
+                }
+            },
+            date: {
+                validators: {
+                    notEmpty: {
+                        message: 'Ngày đặt tiệc không được để trống.'
+                    }
+                }
+            }
+
+        },
+        onSuccess: function(e) {
+
+            var name = $('#txtNameContact').val();
+            var email = $('#txtMail').val();
+            var phone = $('#txtPhoneContact').val();
+            var location = '';
+            var typeId = 1;
+            var emailto = "quyendn84@gmail.com";
+            var dataJSON = { "name": name, "phone": phone, "email": email, "location": location, "emailto": emailto };
+            showLoadingContactImage('content-download', 'frmContentDownload');
+            $.ajax({
+                url: "https://alpha.f5academy.net/api/OOCservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function(states) {
+                    $('#frmDattiec').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-download', 'frmContentDownload');
+
+                },
+                error: function(ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-download', 'frmContentDownload');
+                },
+                complete: function(jqXHR, textStatus) {
+                    $('#txtNameContact').val('');
+                    $("#txtMail").val('');
+                    $('#txtPhoneContact').val('');
+                    $('#cboLocation').val('');
+                    $('#frmDattiec').bootstrapValidator('resetForm', true);
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-download', 'frmContentDownload');
+                    window.location.href = "https://ooc.vn/phan-mem-ooc/dang-ky-thanh-cong.html";
+
+                }
+            });
+        }
+    }).on('success.form.fv', function(e) {
+
+    });
+
+    function showLoadingImage() {
+
+        $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
+        $('#formContentContact').hide();
+    }
+
+    function hideLoadingImage() {
+        $('#formContentContact').show();
+        $('#loading-image').remove();
+    }
+
+    function showLoadingContactImage(contentLoading, frmContent) {
+
+        $('#' + contentLoading).empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
+        $('#' + frmContent).hide();
+    }
+
+    function hideLoadingContactImage(contentLoading, frmContent) {
+        $('#' + frmContent).show();
+        $('#loading-image').remove();
+    }
+});
--- tms/js/form-validation.js
+++ tms/js/form-validation.js
@@ -68,13 +68,14 @@ $(document).ready(function() {
             var name = $('#txtName').val();
             var phone = $('#txtPhone').val();
             var email = $('#txtEmail').val();
-            var emailto = "thenv202@gmail.com";
+            var emailto = "quyendn84@gmail.com";
             var typeId = 1;
             var webdomain = "f5academy";
-            var dataJSON = { "name": name, "email": email, "phone": phone, "emailto": emailto }
+            var rederect = false;
+            var dataJSON = { "name": name, "email": email, "phone": phone, "typeId": typeId, "emailto": emailto }
             showLoadingContactImage('content-mobile','formContentContactMobile');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/F5service",
+                url: "http://localhost:50623/api/TSMservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
@@ -83,6 +84,7 @@ $(document).ready(function() {
                 success: function (states) {
                     $('#frmMobile').bootstrapValidator('resetForm', true);
                     hideLoadingContactImage('formContentContactMobile', 'content-mobile');
+                    rederect = true;
                 },
                 error: function (ex) {
                     toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
@@ -93,7 +95,83 @@ $(document).ready(function() {
                     $("#txtPhone").val('');
                     $('#txtEmail').val('');
                     $('#frmMobile').bootstrapValidator('resetForm', true);
-                    $("#thanks").show();
+                    if (rederect == true)
+                        window.location.href = "dang-ky-thanh-cong.html";
+                }
+            });
+        }
+    }).on('success.form.fv', function (e) {
+
+    });
+    $('#frmContact').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            email_contact: {
+                validators: {
+                    notEmpty: {
+                        message: 'Địa chỉ email không được để trống.'
+                    },
+                    emailAddress: {
+                        message: 'Không đúng định dạng email'
+                    }
+                }
+            },
+            name_contact: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phone_contact: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 11,
+                        message: 'Số điện thoại chỉ có thể là 10 hoặc 11 số.'
+                    }
+                }
+            }
+        },
+        onSuccess: function (e) {
+
+            var name = $('#txtNameContact').val();
+            var phone = $('#txtPhoneContact').val();
+            var email = $('#txtEmailContact').val();
+            var emailto = "quyendn84@gmail.com";
+            var typeId = 2;
+            var webdomain = "f5academy";
+            var rederect = false;
+            var dataJSON = { "name": name, "email": email, "phone": phone, "typeId": typeId, "emailto": emailto }
+            showLoadingContactImage('content-contact', 'formContentContact');
+            $.ajax({
+                url: "http://localhost:50623/api/TSMservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function (states) {
+                    $('#frmMobile').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('formContentContact', 'content-contact');
+                    rederect = true;
+                },
+                error: function (ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-contact', 'formContentContact');
+                },
+                complete: function (jqXHR, textStatus) {
+                    $('#txtNameContact').val('');
+                    $("#txtPhoneContact").val('');
+                    $('#txtEmailContact').val('');
+                    $('#frmContact').bootstrapValidator('resetForm', true);
+                    if (rederect == true)
+                        window.location.href = "dang-ky-thanh-cong.html";
                 }
             });
         }
