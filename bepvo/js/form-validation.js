b7f815f17c6e26ba595d1e9d418c1b443196ff6b
quyendn <quyendn84@gmail.com>
2019-08-21 19:43:16 +0700

    quyendn

--- bepvo/js/form-validation.js
+++ bepvo/js/form-validation.js
@@ -61,19 +61,30 @@ $(document).ready(function() {
                         message: 'Số điện thoại chỉ có thể là 10 hoặc 11 số.'
                     }
                 }
+            },
+            cbomenu: {
+                validators: {
+                    notEmpty: {
+                        message: 'Món ăn chưa được chọn.'
+                    }
+                }
             }
         },
         onSuccess: function (e) {
 
             var name = $('#txtName').val();
             var phone = $('#txtPhone').val();
-            var description = $('#txtAddress').val();
+            var product = $('#cbomenu').val();
+            var description = $('#txtDescription').val();
             var emailto = "quyendn84@gmail.com";
             var webdomain = "f5academy";
-            var dataJSON = { "name": name, "phone": phone, "description": description, "emailto": emailto }
+            var dataJSON = { "name": name, "phone": phone, "product": product, "description": description, "emailto": emailto }
+            var check = checkPhoneNumber();
+            if (!check)
+                return;
             showLoadingContactImage('content-mobile','formContentContactMobile');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/Richyservice",
+                url: "https://alpha.f5academy.net/api/Bepvoservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
@@ -90,18 +101,35 @@ $(document).ready(function() {
                 complete: function (jqXHR, textStatus) {
                     $('#txtName').val('');
                     $("#txtPhone").val('');
-                    $('#txtAddress').val('');
+                    $('#txtDescription').val('');
                     $('#frmMobile').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký.</br>Chúng tôi sẽ liên hệ tư vấn sớm nhất!', {
+                    toastr.success('Cảm ơn bạn đã đăng ký.</br>Bếp sẽ sớm liên hệ để chốt đơn với Quý khách hàng!', {
                         timeOut: 5000
                     })
-                    window.location.href = "https://quyendn.github.io/richy/dang-ky-thanh-cong.html";
+                    window.location.href = "https://quyendn.github.io/bepvo/dang-ky-thanh-cong.html";
                 }
             });
         }
     }).on('success.form.fv', function (e) {
 
     });
+    function checkPhoneNumber() {
+        var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        var mobile = $('#txtPhone').val();
+        if (mobile !== '') {
+            if (vnf_regex.test(mobile) == false) {
+                toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
+                $("#txtPhone").focus();
+                return false;
+
+            } else {
+                return true;
+            }
+        } else {
+            toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            return false;
+        }
+    }
     function showLoadingImage() {
 
         $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
