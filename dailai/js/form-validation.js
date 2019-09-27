a47bfae5b61f6d1f69838457ca64a89c9ce3bdd2
quyendn <quyendn@vnpt.vn>
2019-09-27 16:44:45 +0700

    quyendn

--- dailai/js/form-validation.js
+++ dailai/js/form-validation.js
@@ -78,7 +78,7 @@ $(document).ready(function() {
             var dataJSON = { "name": name, "phone": phone, "email": email, "description": description, "emailto": emailto };
             showLoadingContactImage('content-download', 'frmContentReg');
             $.ajax({
-                url: "http://localhost:50623/api/Flamingoservice",
+                url: "http://alpha.f5academy.net/api/Flamingoservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
