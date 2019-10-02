5b9a0af4881d40c38fc87add4f3449d898191bab
quyendn <quyendn84@gmail.com>
2019-10-02 21:25:07 +0700

    quyendn

--- seabank/js/form-validation.js
+++ seabank/js/form-validation.js
@@ -80,78 +80,161 @@ $(document).ready(function() {
         },
         onSuccess: function (e) {
 
-            var name = $('#txtName').val();
-            var email = $('#txtEmail').val();
-            var phone = $('#txtPhone').val();
-            var birthdate = $("#txtBrith").val();
-            var address = $('#txtAdd').val();
-            var isSend = 0;
-            var emailto = "quyendn84@gmail.com";
-            var nguon1 = $("input[name='chk1[]']:checked").val();
-            if (isEmpty(nguon1))
-                nguon1 = "";
-            var nguon2 = $("input[name='chk2[]']:checked").val();
-            if (isEmpty(nguon2))
-                nguon2 = "";
-            var nguon3 = $("input[name='chk3[]']:checked").val();
-            if (isEmpty(nguon3))
-                nguon3 = "";
-            var nguon4 = $("input[name='chk4[]']:checked").val();
-            if (isEmpty(nguon4))
-                nguon4 = "";
-            var nguon5 = $("input[name='chk5[]']:checked").val();
-            if (isEmpty(nguon5))
-                nguon5 = "";
-            var nguon6 = $("input[name='chk6[]']:checked").val();
-            if (isEmpty(nguon6))
-                nguon6 = "";
-            var check = checkPhoneNumber();
-            if (!check)
-                return;
-            var dataJSON = {
-                "name": name,
-                "birthdate": birthdate,
-                "phone": phone,
-                "email": email,
-                "address": address,
-                "info1": nguon1,
-                "info2": nguon2,
-                "info3": nguon3,
-                "info4": nguon4,
-                "info5": nguon5,
-                "info6": nguon6,
-                "emailto": emailto
-            };
-            showLoadingContactImage('content-download', 'frmContentReg');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/Seabankservice",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function (states) {
-                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-download', 'frmContentReg');
-                 },
-                error: function (ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-download', 'frmContentReg');
-                },
-                complete: function (jqXHR, textStatus) {
-                    $('#txtName').val('');
-                    $("#txtEmail").val('');
-                    $('#txtPhone').val('');
-                    $('#txtDescription').val('');
-                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-download', 'frmContentReg');
-                    window.location.href = "/dang-ky-thanh-cong.html";
-                }
-            });
+            
         }
     }).on('success.form.fv', function (e) {
 
+    });
+    $("#btnSendInfo").on("click", function () {
+        var name = $('#txtName').val();
+        if (isEmpty(name)) {
+            toastr.error('Bạn cần điền họ và tên.', { timeOut: 5000 })
+            $("#txtName").focus();
+            return;
+        }
+        var birthdate = $("#txtBrith").val();
+        if (isEmpty(birthdate)) {
+            toastr.error('Bạn cần điền năm sinh.', { timeOut: 5000 })
+            $("#txtBrith").focus();
+            return;
+        }
+        
+        var phone = $('#txtPhone').val();
+        if (isEmpty(phone)) {
+            toastr.error('Bạn cần điền số điện thoại.', { timeOut: 5000 })
+            $("#txtPhone').").focus();
+            return;
+        }
+        var email = $('#txtEmail').val();
+        if (isEmpty(email)) {
+            toastr.error('Bạn cần điền email.', { timeOut: 5000 })
+            $("#txtEmail").focus();
+            return;
+        }
+        var address = $('#txtAdd').val();
+        if (isEmpty(address)) {
+            toastr.error('Bạn cần điền địa chỉ.', { timeOut: 5000 })
+            $("#txtAdd").focus();
+            return;
+        }
+        var isSend = 0;
+        var emailto = "quyendn84@gmail.com";
+        var nguon1 = $("input[name='chk1[]']:checked").val();
+        if (isEmpty(nguon1))
+            nguon1 = "";
+        var nguon2 = $("input[name='chk2[]']:checked").val();
+        if (isEmpty(nguon2))
+            nguon2 = "";
+        var nguon3 = $("input[name='chk3[]']:checked").val();
+        if (isEmpty(nguon3))
+            nguon3 = "";
+        var nguon4 = $("input[name='chk4[]']:checked").val();
+        if (isEmpty(nguon4))
+            nguon4 = "";
+        var nguon5 = $("input[name='chk5[]']:checked").val();
+        if (isEmpty(nguon5))
+            nguon5 = "";
+        var nguon6 = $("input[name='chk6[]']:checked").val();
+        if (isEmpty(nguon6))
+            nguon6 = "";
+        var check = checkPhoneNumber();
+        if (!check)
+            return;
+        var dataJSON = {
+            "name": name,
+            "birthdate": birthdate,
+            "phone": phone,
+            "email": email,
+            "address": address,
+            "info1": nguon1,
+            "info2": nguon2,
+            "info3": nguon3,
+            "info4": nguon4,
+            "info5": nguon5,
+            "info6": nguon6,
+            "emailto": emailto
+        };
+        showLoadingContactImage('content-download', 'frmContentReg');
+        $.ajax({
+            url: "https://alpha.f5academy.net/api/Seabankservice",
+            type: "Post",
+            async: false,
+            data: dataJSON,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'jsonp',
+            success: function (states) {
+                $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                hideLoadingContactImage('content-download', 'frmContentReg');
+            },
+            error: function (ex) {
+                toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                hideLoadingContactImage('content-download', 'frmContentReg');
+            },
+            complete: function (jqXHR, textStatus) {
+                $('#txtName').val('');
+                $("#txtEmail").val('');
+                $('#txtPhone').val('');
+                $('#txtDescription').val('');
+                $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                hideLoadingContactImage('content-download', 'frmContentReg');
+                window.location.href = "/dang-ky-thanh-cong.html";
+            }
+        });
+    });
+    $("#btn-register").on("click", function () {
+        var phone = $('#txtPhonePopup').val();
+        var check = checkPhoneNumber2();
+        if (!check)
+            return;
+        var email = $('#txtEmailPopup').val();
+        if (isEmpty(email))
+        {
+            toastr.error('Bạn cần điền email.', { timeOut: 5000 })
+            $("#txtEmailPopup").focus();
+            return;
+        }
+        var address = $('#txtAddPopup').val();
+        if (isEmpty(address)) {
+            toastr.error('Bạn cần điền nơi làm việc.', { timeOut: 5000 })
+            $("#txtAddPopup").focus();
+            return;
+        }
+        var isSend = 0;
+        var emailto = "quyendn84@gmail.com";
+        var dataJSON = {
+            "phone": phone,
+            "email": email,
+            "address": address,
+            "emailto": emailto
+        };
+        showLoadingContactImage('content-popup-download', 'frmContent');
+        $.ajax({
+            url: "https://alpha.f5academy.net/api/Seabankvoucher",
+            type: "Post",
+            async: false,
+            data: dataJSON,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'jsonp',
+            success: function (states) {
+                $('#frmRegPopupDownload').bootstrapValidator('resetForm', true);
+                hideLoadingContactImage('content-popup-download', 'frmContent');
+            },
+            error: function (ex) {
+                toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                hideLoadingContactImage('content-popup-download', 'frmContent');
+            },
+            complete: function (jqXHR, textStatus) {
+                $('#txtName').val('');
+                $("#txtEmail").val('');
+                $('#txtPhone').val('');
+                $('#txtDescription').val('');
+                $('#frmRegPopupDownload').bootstrapValidator('resetForm', true);
+                toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                hideLoadingContactImage('content-popup-download', 'frmContent');
+                $("#popup").hide();
+            }
+        });
     });
     $('#frmRegPopupDownload').bootstrapValidator({
         message: 'This value is not valid',
@@ -213,42 +296,10 @@ $(document).ready(function() {
             var check = checkPhoneNumber2();
             if (!check)
                 return;
-            var dataJSON = {
-                "phone": phone,
-                "email": email,
-                "address": address,
-                "emailto": emailto
-            };
-            showLoadingContactImage('content-popup-download', 'frmContent');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/Seabankvoucher",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function (states) {
-                    $('#frmRegPopupDownload').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-popup-download', 'frmContent');
-                },
-                error: function (ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-popup-download', 'frmContent');
-                },
-                complete: function (jqXHR, textStatus) {
-                    $('#txtName').val('');
-                    $("#txtEmail").val('');
-                    $('#txtPhone').val('');
-                    $('#txtDescription').val('');
-                    $('#frmRegPopupDownload').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-popup-download', 'frmContent');
-                    $("#popup").hide();
-                }
-            });
+            
         }
     }).on('success.form.fv', function (e) {
-
+       
     });
     function showLoadingImage() {
 
@@ -288,7 +339,9 @@ $(document).ready(function() {
                 return true;
             }
         } else {
+
             toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            $("#txtPhone").focus();
             return false;
         }
     }
@@ -306,6 +359,7 @@ $(document).ready(function() {
             }
         } else {
             toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            $("#txtPhonePopup").focus();
             return false;
         }
     }
