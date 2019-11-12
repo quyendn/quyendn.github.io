80692b7f3baff836d0ba8a69340fc0ab30aa5b22
quyendn <quyendn84@gmail.com>
2019-11-12 22:40:40 +0700

    quyendn

--- toyotav6/js/form-validation.js
+++ toyotav6/js/form-validation.js
@@ -27,8 +27,8 @@ $(document).ready(function() {
         // Indicate where the error messages are shown.
         // Tooltip, Popover, Custom Container.
         // =================================================================
-    
-    $('#frmMobile').bootstrapValidator({
+
+    $('#frmRegDownload').bootstrapValidator({
         message: 'This value is not valid',
         excluded: [':disabled'],
         feedbackIcons: faIcon,
@@ -57,70 +57,265 @@ $(document).ready(function() {
                     },
                     stringLength: {
                         min: 10,
-                        max: 11,
-                        message: 'Số điện thoại chỉ có thể là 10 hoặc 11 số.'
+                        max: 10,
+                        message: 'Số điện thoại chỉ có thể là 10 số.'
+                    }
+                }
+            },
+            cboCity: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn Tỉnh/thành phố'
+                    }
+                }
+            },
+            cboPartner: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn Showroom/Đại lý'
                     }
                 }
             }
         },
-        onSuccess: function (e) {
+        onSuccess: function(e) {
 
             var name = $('#txtName').val();
-            var phone = $('#txtPhone').val();
             var email = $('#txtEmail').val();
-            var emailto = "thenv202@gmail.com";
+            var phone = $('#txtPhone').val();
+            var partner = $('#cboCars').val();
+            var emailto = "quyendn84@gmail.com";
             var typeId = 1;
-            var webdomain = "f5academy";
-            var dataJSON = { "name": name, "email": email, "phone": phone, "emailto": emailto }
-            showLoadingContactImage('content-mobile','formContentContactMobile');
+            var check = checkPhoneNumber();
+            if (!check)
+                return;
+            var dataJSON = { "name": name, "phone": phone, "showroom": partner, "email": email, "emailto": emailto, "typeId": typeId };
+            showLoadingContactImage('content-register', 'frmContentReg');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/F5service",
+                url: "https://alpha.f5academy.net/api/ToyotaVehicleservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
                 contentType: 'application/json; charset=utf-8',
                 dataType: 'jsonp',
-                success: function (states) {
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('formContentContactMobile', 'content-mobile');
+                success: function(states) {
+                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-register', 'frmContentReg');
                 },
-                error: function (ex) {
+                error: function(ex) {
                     toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile','formContentContactMobile');
+                    hideLoadingContactImage('content-register', 'frmContentReg');
                 },
-                complete: function (jqXHR, textStatus) {
+                complete: function(jqXHR, textStatus) {
                     $('#txtName').val('');
-                    $("#txtPhone").val('');
-                    $('#txtEmail').val('');
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    $("#thanks").show();
+                    $("#txtEmail").val('');
+                    $('#txtPhone').val('');
+                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                    window.location.href = "https://quyendn.github.io/toyotav6/dang-ky-thanh-cong.html";
                 }
             });
         }
-    }).on('success.form.fv', function (e) {
+    }).on('success.form.fv', function(e) {
 
     });
+    $('#frmRegDownloadSub').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            emailsub: {
+                validators: {
+                    notEmpty: {
+                        message: 'Địa chỉ email không được để trống.'
+                    },
+                    emailAddress: {
+                        message: 'Không đúng định dạng email'
+                    }
+                }
+            },
+            namesub: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phonesub: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 10,
+                        message: 'Số điện thoại chỉ có thể là 10 số.'
+                    }
+                }
+            },
+            cboSubCity: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn Tỉnh/thành phố'
+                    }
+                }
+            },
+            cboSubPartner: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn Showroom/Đại lý'
+                    }
+                }
+            }
+        },
+        onSuccess: function(e) {
+
+            var name = $('#txtNameSub').val();
+            var email = $('#txtEmailSub').val();
+            var phone = $('#txtPhoneSub').val();
+            var city = $('#cboSubCity').val();
+            var partner = $('#cboSubPartner').val();
+            var emailto = "quyendn84@gmail.com";
+            var typeId = 1;
+            var check = checkPhoneNumber2();
+            if (!check)
+                return;
+            var dataJSON = { "city": city, "partner": partner, "name": name, "phone": phone, "email": email, "emailto": emailto, "typeId": typeId };
+            showLoadingContactImage('content-register-sub', 'frmContentReg');
+            $.ajax({
+                url: "https://alpha.f5academy.net/api/Mazdaservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function(states) {
+                    $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
+                },
+                error: function(ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
+                },
+                complete: function(jqXHR, textStatus) {
+                    $('#txtNameSub').val('');
+                    $("#txtEmailSub").val('');
+                    $('#txtPhoneSub').val('');
+                    $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
+                    window.location.href = "http://cx-8.mazdamotors.vn/dang-ky-thanh-cong.html";
+                }
+            });
+        }
+    }).on('success.form.fv', function(e) {
+
+    });
+    $('#frmMobile').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            emailBrochure: {
+                validators: {
+                    notEmpty: {
+                        message: 'Địa chỉ email không được để trống.'
+                    },
+                    emailAddress: {
+                        message: 'Không đúng định dạng email'
+                    }
+                }
+            }
+        },
+        onSuccess: function(e) {
+            var email = $('#txtEmailBrochure').val();
+            var emailto = "quyendn84@gmail.com";
+            var typeId = 2;
+            var dataJSON = { "city": "", "partner": "", "name": "", "phone": "", "email": email, "emailto": emailto, "typeId": typeId };
+            showLoadingContactImage('content-mobile', 'formContentContactMobile');
+            $.ajax({
+                url: "https://alpha.f5academy.net/api/Mazdaservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function(states) {
+                    $('#frmMobile').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
+                },
+                error: function(ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-mobile', 'frmContentReg');
+                },
+                complete: function(jqXHR, textStatus) {
+                    $("#txtEmailBrochure").val('');
+                    $('#frmMobile').bootstrapValidator('resetForm', true);
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
+                    window.location.href = "http://cx-8.mazdamotors.vn/dang-ky-thanh-cong.html";
+                }
+            });
+        }
+    }).on('success.form.fv', function(e) {
+
+    });
+
     function showLoadingImage() {
 
         $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
         $('#formContentContact').hide();
-
     }
 
     function hideLoadingImage() {
         $('#formContentContact').show();
         $('#loading-image').remove();
     }
-   
-    function showLoadingContactImage(contentLoading,frmContent) {
+
+    function showLoadingContactImage(contentLoading, frmContent) {
 
         $('#' + contentLoading).empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
         $('#' + frmContent).hide();
-
     }
 
     function hideLoadingContactImage(contentLoading, frmContent) {
         $('#' + frmContent).show();
         $('#loading-image').remove();
+    }
+
+    function checkPhoneNumber() {
+        var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        var mobile = $('#txtPhone').val();
+        if (mobile !== '') {
+            if (vnf_regex.test(mobile) == false) {
+                toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
+                $("#txtPhone").focus();
+                return false;
+
+            } else {
+                return true;
+            }
+        } else {
+            toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            return false;
+        }
+    }
+
+    function checkPhoneNumber2() {
+        var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        var mobile = $('#txtPhoneSub').val();
+        if (mobile !== '') {
+            if (vnf_regex.test(mobile) == false) {
+                toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
+                $("#txtPhoneSub").focus();
+                return false;
+
+            } else {
+                return true;
+            }
+        } else {
+            toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            return false;
+        }
     }
 });
