7a089013870be96024985bac3eb3abc655506c1c
quyendn <quyendn84@gmail.com>
2019-11-18 12:29:21 +0700

    quyendn

--- xuanhoa/js/form-validation.js
+++ xuanhoa/js/form-validation.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
                     $('#frmRegDownload').bootstrapValidator('resetForm', true);
                     toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
                     hideLoadingContactImage('content-register', 'frmContentReg');
-                    window.location.href = "/dang-ky-thanh-cong.html";
+                    window.location.href = "https://quyendn.github.io/toyotav6/dang-ky-thanh-cong.html";
                 }
             });
         }
