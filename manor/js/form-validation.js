4d8a65ae5a3f2220d1f5b5f6aa8c20acd76dac4a
quyendn <quyendn84@gmail.com>
2019-11-20 09:30:43 +0700

    quyendn

--- manor/js/form-validation.js
+++ manor/js/form-validation.js
@@ -82,16 +82,16 @@ $(document).ready(function() {
             var name = $('#txtName').val();
             var email = $('#txtEmail').val();
             var phone = $('#txtPhone').val();
-            var partner = $('#cboCars').val();
+            var description = '';
             var emailto = "quyendn84@gmail.com";
             var typeId = 1;
             var check = checkPhoneNumber();
             if (!check)
                 return;
-            var dataJSON = { "name": name, "phone": phone, "showroom": partner, "email": email, "emailto": emailto, "typeId": typeId };
+            var dataJSON = { "name": name, "phone": phone,  "email": email,"description": description, "emailto": emailto, "typeId": typeId };
             showLoadingContactImage('content-register', 'frmContentReg');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/ToyotaVehicleservice",
+                url: "https://alpha.f5academy.net/api/TheManorservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
@@ -112,7 +112,7 @@ $(document).ready(function() {
                     $('#frmRegDownload').bootstrapValidator('resetForm', true);
                     toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
                     hideLoadingContactImage('content-register', 'frmContentReg');
-                    window.location.href = "https://quyendn.github.io/toyotav6/dang-ky-thanh-cong.html";
+                    window.location.href = "https://quyendn.github.io/manor/dang-ky-thanh-cong.html";
                 }
             });
         }
@@ -173,17 +173,16 @@ $(document).ready(function() {
             var name = $('#txtNameSub').val();
             var email = $('#txtEmailSub').val();
             var phone = $('#txtPhoneSub').val();
-            var city = $('#cboSubCity').val();
-            var partner = $('#cboSubPartner').val();
+            var description = '';
             var emailto = "quyendn84@gmail.com";
             var typeId = 1;
             var check = checkPhoneNumber2();
             if (!check)
                 return;
-            var dataJSON = { "city": city, "partner": partner, "name": name, "phone": phone, "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-register-sub', 'frmContentReg');
+            var dataJSON = { "name": name, "phone": phone,  "email": email,"description": description, "emailto": emailto, "typeId": typeId };
+            showLoadingContactImage('content-register-sub', 'frmContentRegSub');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/Mazdaservice",
+                url: "https://alpha.f5academy.net/api/TheManorservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
@@ -204,19 +203,19 @@ $(document).ready(function() {
                     $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
                     toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
                     hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                    window.location.href = "http://cx-8.mazdamotors.vn/dang-ky-thanh-cong.html";
+                    window.location.href = "https://quyendn.github.io/manor/dang-ky-thanh-cong.html";
                 }
             });
         }
     }).on('success.form.fv', function(e) {
 
     });
-    $('#frmMobile').bootstrapValidator({
+    $('#frmRegContact').bootstrapValidator({
         message: 'This value is not valid',
         excluded: [':disabled'],
         feedbackIcons: faIcon,
         fields: {
-            emailBrochure: {
+            emailcontact: {
                 validators: {
                     notEmpty: {
                         message: 'Địa chỉ email không được để trống.'
@@ -225,35 +224,77 @@ $(document).ready(function() {
                         message: 'Không đúng định dạng email'
                     }
                 }
+            },
+            namecontact: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phonecontact: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 10,
+                        message: 'Số điện thoại chỉ có thể là 10 số.'
+                    }
+                }
+            },
+            cboSubCity: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn Tỉnh/thành phố'
+                    }
+                }
+            },
+            cboSubPartner: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn Showroom/Đại lý'
+                    }
+                }
             }
         },
         onSuccess: function(e) {
-            var email = $('#txtEmailBrochure').val();
+
+            var name = $('#txtNameContact').val();
+            var email = $('#txtEmailContact').val();
+            var phone = $('#txtPhoneContact').val();
+            var description = '';
             var emailto = "quyendn84@gmail.com";
-            var typeId = 2;
-            var dataJSON = { "city": "", "partner": "", "name": "", "phone": "", "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-mobile', 'formContentContactMobile');
+            var typeId = 1;
+            var check = checkPhoneNumber3();
+            if (!check)
+                return;
+            var dataJSON = { "name": name, "phone": phone,  "email": email,"description": description, "emailto": emailto, "typeId": typeId };
+            showLoadingContactImage('content-register-contact', 'frmContentRegContact');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/Mazdaservice",
+                url: "https://alpha.f5academy.net/api/TheManorservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
                 contentType: 'application/json; charset=utf-8',
                 dataType: 'jsonp',
                 success: function(states) {
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
+                    $('#frmRegContact').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-register-contact', 'frmContentRegContact');
                 },
                 error: function(ex) {
                     toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile', 'frmContentReg');
+                    hideLoadingContactImage('content-register-contact', 'frmContentRegContact');
                 },
                 complete: function(jqXHR, textStatus) {
-                    $("#txtEmailBrochure").val('');
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
+                    $('#txtNameSub').val('');
+                    $("#txtEmailSub").val('');
+                    $('#txtPhoneSub').val('');
+                    $('#frmRegContact').bootstrapValidator('resetForm', true);
                     toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
-                    window.location.href = "http://cx-8.mazdamotors.vn/dang-ky-thanh-cong.html";
+                    hideLoadingContactImage('content-register-contact', 'frmContentRegContact');
+                    window.location.href = "https://quyendn.github.io/manor/dang-ky-thanh-cong.html";
                 }
             });
         }
@@ -308,6 +349,23 @@ $(document).ready(function() {
             if (vnf_regex.test(mobile) == false) {
                 toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
                 $("#txtPhoneSub").focus();
+                return false;
+
+            } else {
+                return true;
+            }
+        } else {
+            toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            return false;
+        }
+    }
+    function checkPhoneNumber3() {
+        var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        var mobile = $('#txtPhoneContact').val();
+        if (mobile !== '') {
+            if (vnf_regex.test(mobile) == false) {
+                toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
+                $("#txtPhoneContact").focus();
                 return false;
 
             } else {
