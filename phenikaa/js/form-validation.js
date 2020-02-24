7146d407281a4e5a6aa3d91f894929f3405e73bc
quyendn <quyendn@vnpt.vn>
2020-02-24 17:05:27 +0700

    quyendn

---
+++ phenikaa/js/form-validation.js
@@ -0,0 +1,194 @@
+﻿// Form-Validation.js
+// ====================================================================
+// This file should not be included in your project.
+// This is just a sample how to initialize plugins or components.
+//
+// - ThemeOn.net -
+
+$(document).ready(function() {
+
+
+    // FORM VALIDATION
+    // =================================================================
+    // Require Bootstrap Validator
+    // http://bootstrapvalidator.com/
+    // =================================================================
+    // FORM VALIDATION FEEDBACK ICONS
+    // =================================================================
+    var faIcon = {
+            valid: 'fa fa-check-circle fa-lg text-success',
+            invalid: 'fa fa-times-circle fa-lg',
+            validating: 'fa fa-refresh'
+        }
+        // FORM VALIDATION ON ACCORDION
+        // =================================================================
+        // FORM VALIDATION CUSTOM ERROR CONTAINER
+        // =================================================================
+        // Indicate where the error messages are shown.
+        // Tooltip, Popover, Custom Container.
+        // =================================================================
+
+    $('#frmRegDownload').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            cboStatus: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần lựa chọn nhu cầu.'
+                    }
+                }
+            },
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 11,
+                        message: 'Số điện thoại chỉ có thể là 10 hoặc 11 số.'
+                    }
+                }
+            }
+        },
+        onSuccess: function(e) {
+
+            var name = $('#txtName').val();
+            var phone = $('#txtPhone').val();
+            var email = $('#cboStatus').val();
+            var emailto = "quyendn84@gmail.com";
+            var dataJSON = { "name": name, "phone": phone, "email": email, "emailto": emailto }
+            showLoadingContactImage('content-register', 'frmContentReg');
+            $.ajax({
+                url: "https://alpha.f5academy.net//api/NguuHoangservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function(states) {
+                    $('#frmMobile').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                },
+                error: function(ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                },
+                complete: function(jqXHR, textStatus) {
+                    $('#txtName').val('');
+                    $("#txtPhone").val('');
+                    $('#txtMail').val('');
+                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                    location.href = "https://nguuhoangthanhtam.vn/nguuhoang/dang-ky-thanh-cong.html";
+                }
+            });
+        }
+    }).on('success.form.fv', function(e) {
+
+    });
+    $('#frmSaleOff').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            cboStatusSaleOff: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần lựa chọn nhu cầu.'
+                    }
+                }
+            },
+            namesaleoff: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phonesaleoff: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 11,
+                        message: 'Số điện thoại chỉ có thể là 10 hoặc 11 số.'
+                    }
+                }
+            }
+        },
+        onSuccess: function(e) {
+
+            var name = $('#txtNameSaleoff').val();
+            var phone = $('#txtPhoneSaleoff').val();
+            var email = $('#cboStatusSaleOff').val();
+            var emailto = "quyendn84@gmail.com";
+            var dataJSON = { "name": name, "phone": phone, "email": email, "emailto": emailto }
+            showLoadingContactImage('content-loading', 'frmContentRegSaleOff');
+            $.ajax({
+                url: "https://alpha.f5academy.net//api/NguuHoangservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function(states) {
+                    $('#frmSaleOff').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-loading', 'frmContentRegSaleOff');
+                },
+                error: function(ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-loading', 'frmContentRegSaleOff');
+                },
+                complete: function(jqXHR, textStatus) {
+                    $('#txtName').val('');
+                    $("#txtPhone").val('');
+                    $('#txtMail').val('');
+                    $('#txtBirthday').val('');
+                    $('#frmSaleOff').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-loading', 'frmContentRegSaleOff');
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 });
+                    location.href = "https://nguuhoangthanhtam.vn/nguuhoang/dang-ky-thanh-cong.html";
+                }
+            });
+        }
+    }).on('success.form.fv', function(e) {
+
+    });
+
+    function showLoadingImage() {
+
+        $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
+        $('#formContentContact').hide();
+
+    }
+
+    function hideLoadingImage() {
+        $('#formContentContact').show();
+        $('#loading-image').remove();
+    }
+
+    function showLoadingContactImage(contentLoading, frmContent) {
+
+        $('#' + contentLoading).empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
+        $('#' + frmContent).hide();
+
+    }
+
+    function hideLoadingContactImage(contentLoading, frmContent) {
+        $('#' + frmContent).show();
+        $('#loading-image').remove();
+    }
+});
