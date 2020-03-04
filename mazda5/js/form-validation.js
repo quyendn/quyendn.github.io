4b4d69fb8f10bce25904fecca9d0979b7ae0275b
quyendn <quyendn84@gmail.com>
2020-03-04 22:09:51 +0700

    quyendn

--- mazda5/js/form-validation.js
+++ mazda5/js/form-validation.js
@@ -106,7 +106,7 @@ $(document).ready(function() {
                     $('#frmRegDownload').bootstrapValidator('resetForm', true);
                     toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
                     hideLoadingContactImage('content-register', 'frmContentReg');
-                    window.location.href = "https://quyendn.github.io/mazda4/dang-ky-thanh-cong.html";
+                    window.location.href = "https://quyendn.github.io/mazda5/dang-ky-thanh-cong.html";
                 }
             });
         }
