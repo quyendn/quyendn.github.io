3b3d5d6096e1d4f3559a124bb6b426dd2bb86ac8
Quyen Nguyen <quyendn84@gmail.com>
2020-07-20 10:42:25 +0700

    quyennd

--- traveller/js/form-validation.js
+++ traveller/js/form-validation.js
@@ -28,220 +28,106 @@ $(document).ready(function() {
         // Tooltip, Popover, Custom Container.
         // =================================================================
 
-    $('#frmRegDownload').bootstrapValidator({
-        message: 'This value is not valid',
-        excluded: [':disabled'],
-        feedbackIcons: faIcon,
-        fields: {
-            // email: {
-            //     validators: {
-            //         notEmpty: {
-            //             message: 'Địa chỉ email không được để trống.'
-            //         },
-            //         emailAddress: {
-            //             message: 'Không đúng định dạng email'
-            //         }
-            //     }
-            // },
-          
-            phone: {
-                validators: {
-                    notEmpty: {
-                        message: 'Điện thoại không được để trống.'
-                    },
-                    stringLength: {
-                        min: 10,
-                        max: 10,
-                        message: 'Số điện thoại chỉ có thể là 10 số.'
+        $('#frmRegDownload').bootstrapValidator({
+            message: 'This value is not valid',
+            excluded: [':disabled'],
+            feedbackIcons: faIcon,
+            fields: {
+                // email: {
+                //      validators: {
+                //          notEmpty: {
+                //              message: 'Địa chỉ email không được để trống.'
+                //          },
+                //          emailAddress: {
+                //              message: 'Không đúng định dạng email'
+                //          }
+                //      }
+                //  },
+                phone: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Điện thoại không được để trống.'
+                        },
+                        stringLength: {
+                            min: 10,
+                            max: 10,
+                            message: 'Số điện thoại chỉ có thể là 10 số.'
+                        }
                     }
-                }
+                },
+                name: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Họ tên không được để trống.'
+                        }
+                    }
+                },
+                cboCity: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Quý khách cần chọn Tỉnh/thành phố.'
+                        }
+                    }
+                },
+                cboCar: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Quý khách cần chọn dòng xe.'
+                        }
+                    }
+                },
+            },
+            onSuccess: function(e) {
+    
+                var name = $('#txtName').val();
+                var email = $('#txtEmail').val();
+                var phone = $('#txtPhone').val();
+                var vehice = $('#cboCar').val();
+                var city = $('#cboCity').val();
+                var emailto = "quyendn84@gmail.com";
+                var isSendMail = 0;
+                var url_source = "lp_traveller";
+                var source = getUrlParameter('utm_source');
+                if (isEmpty(source))
+                    url_source = "lp_traveller";
+                else
+                    url_source = source;
+                var check = checkPhoneNumber();
+                if (!check)
+                    return;
+                var dataJSON = { "name": name, "phone": phone,"email": email, "source": url_source, "vehice": vehice, "city": city, "isSendMail": isSendMail, "emailto": emailto };
+                showLoadingContactImage('content-register', 'frmContentReg');
+                $.ajax({
+                    url: "https://alpha.f5academy.net/api/Travellerservice",
+                    type: "Post",
+                    async: false,
+                    data: dataJSON,
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'jsonp',
+                    success: function(states) {
+                        $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                        hideLoadingContactImage('content-register', 'frmContentReg');
+                        $('#formModal').hide();
+                    },
+                    error: function(ex) {
+                        toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                        hideLoadingContactImage('content-register', 'frmContentReg');
+                        $('#formModal').hide();
+                    },
+                    complete: function(jqXHR, textStatus) {
+                        $('#txtName').val('');
+                        $("#txtEmail").val('');
+                        $('#txtPhone').val('');
+                        $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                        toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                        hideLoadingContactImage('content-register', 'frmContentReg');
+                        window.location.href = "https://quyendn.github.io/traveller/dang-ky-thanh-cong.html";
+                    }
+                });
             }
-        },
-        onSuccess: function(e) {
-
-            var name = $('#txtName').val();
-            var email = $('#txtEmail').val();
-            var phone = $('#txtPhone').val();
-            var city = $('#cboCity').val();
-            var partner = "new-mazda6";
-            var emailto = "quyendn84@gmail.com";
-            var typeId = 1;
-            var check = checkPhoneNumber();
-            if (!check)
-                return;
-            var dataJSON = { "city": city, "partner": partner, "name": name, "phone": phone, "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-register', 'frmContentReg');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/TravellerNewservice",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function(states) {
-                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-register', 'frmContentReg');
-                },
-                error: function(ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-register', 'frmContentReg');
-                },
-                complete: function(jqXHR, textStatus) {
-                    $('#txtName').val('');
-                    $("#txtEmail").val('');
-                    $('#txtPhone').val('');
-                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-register', 'frmContentReg');
-                    window.location.href = "https://quyendn.github.io/new-mazda6/dang-ky-thanh-cong.html";
-                }
-            });
-        }
-    }).on('success.form.fv', function(e) {
-
-    });
-    $('#frmRegDownloadSub').bootstrapValidator({
-        message: 'This value is not valid',
-        excluded: [':disabled'],
-        feedbackIcons: faIcon,
-        fields: {
-            emailsub: {
-                validators: {
-                    notEmpty: {
-                        message: 'Địa chỉ email không được để trống.'
-                    },
-                    emailAddress: {
-                        message: 'Không đúng định dạng email'
-                    }
-                }
-            },
-            namesub: {
-                validators: {
-                    notEmpty: {
-                        message: 'Họ tên không được để trống.'
-                    }
-                }
-            },
-            phonesub: {
-                validators: {
-                    notEmpty: {
-                        message: 'Điện thoại không được để trống.'
-                    },
-                    stringLength: {
-                        min: 10,
-                        max: 10,
-                        message: 'Số điện thoại chỉ có thể là 10 số.'
-                    }
-                }
-            },
-            cboSubCity: {
-                validators: {
-                    notEmpty: {
-                        message: 'Cần chọn Tỉnh/thành phố'
-                    }
-                }
-            },
-            cboSubPartner: {
-                validators: {
-                    notEmpty: {
-                        message: 'Cần chọn Showroom/Đại lý'
-                    }
-                }
-            }
-        },
-        onSuccess: function(e) {
-
-            var name = $('#txtNameSub').val();
-            var email = $('#txtEmailSub').val();
-            var phone = $('#txtPhoneSub').val();
-            var city = $('#cboSubCity').val();
-            var partner = $('#cboSubPartner').val();
-            var emailto = "quyendn84@gmail.com";
-            var typeId = 1;
-            var check = checkPhoneNumber2();
-            if (!check)
-                return;
-            var dataJSON = { "city": city, "partner": partner, "name": name, "phone": phone, "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-register-sub', 'frmContentReg');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/TravellerNewservice",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function(states) {
-                    $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                },
-                error: function(ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                },
-                complete: function(jqXHR, textStatus) {
-                    $('#txtNameSub').val('');
-                    $("#txtEmailSub").val('');
-                    $('#txtPhoneSub').val('');
-                    $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                    window.location.href = "http://cx-8.mazdamotors.vn/dang-ky-thanh-cong.html";
-                }
-            });
-        }
-    }).on('success.form.fv', function(e) {
-
-    });
-    $('#frmMobile').bootstrapValidator({
-        message: 'This value is not valid',
-        excluded: [':disabled'],
-        feedbackIcons: faIcon,
-        fields: {
-            emailBrochure: {
-                validators: {
-                    notEmpty: {
-                        message: 'Địa chỉ email không được để trống.'
-                    },
-                    emailAddress: {
-                        message: 'Không đúng định dạng email'
-                    }
-                }
-            }
-        },
-        onSuccess: function(e) {
-            var email = $('#txtEmailBrochure').val();
-            var emailto = "quyendn84@gmail.com";
-            var typeId = 2;
-            var dataJSON = { "city": "", "partner": "", "name": "", "phone": "", "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-mobile', 'formContentContactMobile');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/TravellerNewservice",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function(states) {
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
-                },
-                error: function(ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile', 'frmContentReg');
-                },
-                complete: function(jqXHR, textStatus) {
-                    $("#txtEmailBrochure").val('');
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
-                    window.location.href = "http://cx-8.mazdamotors.vn/dang-ky-thanh-cong.html";
-                }
-            });
-        }
-    }).on('success.form.fv', function(e) {
-
-    });
-
+        }).on('success.form.fv', function(e) {
+    
+        });
     function showLoadingImage() {
 
         $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
@@ -299,4 +185,25 @@ $(document).ready(function() {
             return false;
         }
     }
+    function isEmpty(item) {
+        if (item) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
 });
