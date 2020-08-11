e3f93661ac2262a442c33f7d61b3f4e4f4034781
quyendn <quyendn84@gmail.com>
2020-08-11 22:34:45 +0700

    quyennd

--- vio/js/form-validation.js
+++ vio/js/form-validation.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
             cboPartner: {
                 validators: {
                     notEmpty: {
-                        message: 'Cần chọn Showroom/Đại lý'
+                        message: 'Cần chọn thông tin'
                     }
                 }
             }
@@ -82,17 +82,15 @@ $(document).ready(function() {
             var name = $('#txtName').val();
             var email = $('#txtEmail').val();
             var phone = $('#txtPhone').val();
-            var city = $('#cboCity').val();
             var partner = $('#cboPartner').val();
             var emailto = "quyendn84@gmail.com";
-            var typeId = 1;
             var check = checkPhoneNumber();
             if (!check)
                 return;
-            var dataJSON = { "city": city, "partner": partner, "name": name, "phone": phone, "email": email, "emailto": emailto, "typeId": typeId };
+            var dataJSON = { "name": name, "phone": phone, "email": email, "description": partner,  "emailto": emailto };
             showLoadingContactImage('content-register', 'frmContentReg');
             $.ajax({
-                url: "https://alpha.f5academy.net/api/Mazdaservice",
+                url: "https://alpha.f5academy.net/api/Vioservice",
                 type: "Post",
                 async: false,
                 data: dataJSON,
@@ -113,154 +111,15 @@ $(document).ready(function() {
                     $('#frmRegDownload').bootstrapValidator('resetForm', true);
                     toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
                     hideLoadingContactImage('content-register', 'frmContentReg');
-                    window.location.href = "http://quyendn.github.io/mazda/dang-ky-thanh-cong.html";
+                    window.location.href = "https://quyendn.github.io/vio/dang-ky-thanh-cong.html";
                 }
             });
         }
     }).on('success.form.fv', function (e) {
 
     });
-    $('#frmRegDownloadSub').bootstrapValidator({
-        message: 'This value is not valid',
-        excluded: [':disabled'],
-        feedbackIcons: faIcon,
-        fields: {
-            emailsub: {
-                validators: {
-                    notEmpty: {
-                        message: 'Địa chỉ email không được để trống.'
-                    },
-                    emailAddress: {
-                        message: 'Không đúng định dạng email'
-                    }
-                }
-            },
-            namesub: {
-                validators: {
-                    notEmpty: {
-                        message: 'Họ tên không được để trống.'
-                    }
-                }
-            },
-            phonesub: {
-                validators: {
-                    notEmpty: {
-                        message: 'Điện thoại không được để trống.'
-                    },
-                    stringLength: {
-                        min: 10,
-                        max: 10,
-                        message: 'Số điện thoại chỉ có thể là 10 số.'
-                    }
-                }
-            },
-            cboSubCity: {
-                validators: {
-                    notEmpty: {
-                        message: 'Cần chọn Tỉnh/thành phố'
-                    }
-                }
-            },
-            cboSubPartner: {
-                validators: {
-                    notEmpty: {
-                        message: 'Cần chọn Showroom/Đại lý'
-                    }
-                }
-            }
-        },
-        onSuccess: function (e) {
-
-            var name = $('#txtNameSub').val();
-            var email = $('#txtEmailSub').val();
-            var phone = $('#txtPhoneSub').val();
-            var city = $('#cboSubCity').val();
-            var partner = $('#cboSubPartner').val();
-            var emailto = "quyendn84@gmail.com";
-            var typeId = 1;
-            var check = checkPhoneNumber2();
-            if (!check)
-                return;
-            var dataJSON = { "city": city, "partner": partner, "name": name, "phone": phone, "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-register-sub', 'frmContentReg');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/Mazdaservice",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function (states) {
-                    $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                },
-                error: function (ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                },
-                complete: function (jqXHR, textStatus) {
-                    $('#txtNameSub').val('');
-                    $("#txtEmailSub").val('');
-                    $('#txtPhoneSub').val('');
-                    $('#frmRegDownloadSub').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-register-sub', 'frmContentRegSub');
-                    window.location.href = "http://quyendn.github.io/mazda/dang-ky-thanh-cong.html";
-                }
-            });
-        }
-    }).on('success.form.fv', function (e) {
-
-    });
-    $('#frmMobile').bootstrapValidator({
-        message: 'This value is not valid',
-        excluded: [':disabled'],
-        feedbackIcons: faIcon,
-        fields: {
-            emailBrochure: {
-                validators: {
-                    notEmpty: {
-                        message: 'Địa chỉ email không được để trống.'
-                    },
-                    emailAddress: {
-                        message: 'Không đúng định dạng email'
-                    }
-                }
-            }
-        },
-        onSuccess: function (e) {
-            var email = $('#txtEmailBrochure').val();
-            var emailto = "quyendn84@gmail.com";
-            var typeId = 2;
-            var dataJSON = { "city": "", "partner": "", "name": "", "phone": "", "email": email, "emailto": emailto, "typeId": typeId };
-            showLoadingContactImage('content-mobile', 'formContentContactMobile');
-            $.ajax({
-                url: "https://alpha.f5academy.net/api/Mazdaservice",
-                type: "Post",
-                async: false,
-                data: dataJSON,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'jsonp',
-                success: function (states) {
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
-                },
-                error: function (ex) {
-                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile', 'frmContentReg');
-                },
-                complete: function (jqXHR, textStatus) {
-                    $("#txtEmailBrochure").val('');
-                    $('#frmMobile').bootstrapValidator('resetForm', true);
-                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
-                    hideLoadingContactImage('content-mobile', 'formContentContactMobile');
-                    window.location.href = "http://quyendn.github.io/mazda/dang-ky-thanh-cong.html";
-                }
-            });
-        }
-    }).on('success.form.fv', function (e) {
-
-    });
+    
+    
     function showLoadingImage() {
 
         $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
