12221b280df5fbd678e5042b11981d2fe6097cae
Quyen Nguyen <quyendn84@gmail.com>
2020-11-20 16:09:29 +0700

    quyendn

---
+++ lienvietbank/js/form-validation.js
@@ -0,0 +1,201 @@
+﻿// Form-Validation.js
+// ====================================================================
+// This file should not be included in your project.
+// This is just a sample how to initialize plugins or components.
+//
+// - ThemeOn.net -
+
+$(document).ready(function() {
+
+
+    // FORM VALIDATION
+    // =================================================================
+    // Require Bootstrap Validator
+    // http://bootstrapvalidator.com/
+    // =================================================================
+    // FORM VALIDATION FEEDBACK ICONS
+    // =================================================================
+    var faIcon = {
+            valid: 'fa fa-check-circle fa-lg text-success',
+            invalid: 'fa fa-times-circle fa-lg',
+            validating: 'fa fa-refresh'
+        }
+        // FORM VALIDATION ON ACCORDION
+        // =================================================================
+        // FORM VALIDATION CUSTOM ERROR CONTAINER
+        // =================================================================
+        // Indicate where the error messages are shown.
+        // Tooltip, Popover, Custom Container.
+        // =================================================================
+   
+    $('#frmRegDownload').bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: [':disabled'],
+        feedbackIcons: faIcon,
+        fields: {
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Địa chỉ email không được để trống.'
+                    },
+                    emailAddress: {
+                        message: 'Không đúng định dạng email'
+                    }
+                }
+            },
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: 'Họ tên không được để trống.'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    notEmpty: {
+                        message: 'Điện thoại không được để trống.'
+                    },
+                    stringLength: {
+                        min: 10,
+                        max: 10,
+                        message: 'Số điện thoại chỉ có thể là 10 số.'
+                    }
+                }
+            },
+            provider: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn nhập tỉnh/thành phố'
+                    }
+                }
+            },
+            cboDeposit: {
+                validators: {
+                    notEmpty: {
+                        message: 'Cần chọn nhu cầu vay'
+                    }
+                }
+            }
+        },
+        onSuccess: function (e) {
+
+            var name = $('#txtName').val();
+            var email = $('#txtEmail').val();
+            var phone = $('#txtPhone').val();
+            var provider = $('#txtProvider').val();
+            var deposit = $('#cboDeposit').val();
+            var emailto = "quyendn84@gmail.com";
+            var isSend = 0;
+            var check = checkPhoneNumber();
+            if (!check)
+                return;
+            var url_source = "lp_lienviet";
+            var source = getUrlParameter('utm_source');
+            if (isEmpty(source))
+                url_source = "lp_lienviet";
+            else
+                url_source = source;
+            var dataJSON = { "name": name, "phone": phone, "email": email, "provider": provider, "deposit": deposit, "source": url_source, "emailto": emailto, "isSend": isSend };
+            showLoadingContactImage('content-register', 'frmContentReg');
+            $.ajax({
+                url: "https://alpha.f5academy.net/api/LienVietBankservice",
+                type: "Post",
+                async: false,
+                data: dataJSON,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'jsonp',
+                success: function (states) {
+                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                },
+                error: function (ex) {
+                    toastr.error('Đã có lỗi trong quá trình đăng ký, mời bạn thử lại.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                },
+                complete: function (jqXHR, textStatus) {
+                    $('#txtName').val('');
+                    $("#txtEmail").val('');
+                    $('#txtPhone').val('');
+                    $('#frmRegDownload').bootstrapValidator('resetForm', true);
+                    toastr.success('Cảm ơn bạn đã đăng ký, chúng tôi sẽ liên lạc sớm nhất khi nhận thông tin.', { timeOut: 5000 })
+                    hideLoadingContactImage('content-register', 'frmContentReg');
+                }
+            });
+        }
+    }).on('success.form.fv', function (e) {
+
+    });
+    function isEmpty(item) {
+        if (item) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+    function showLoadingImage() {
+
+        $('#content').empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
+        $('#formContentContact').hide();
+    }
+    function hideLoadingImage() {
+        $('#formContentContact').show();
+        $('#loading-image').remove();
+    }
+    function showLoadingContactImage(contentLoading,frmContent) {
+
+        $('#' + contentLoading).empty().append('<div id="loading-image" align="center"><img src="img/ajax-loader.gif" alt="Loading..." /></div>');
+        $('#' + frmContent).hide();
+    }
+    function hideLoadingContactImage(contentLoading, frmContent) {
+        $('#' + frmContent).show();
+        $('#loading-image').remove();
+    }
+    function checkPhoneNumber() {
+        var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        var mobile = $('#txtPhone').val();
+        if (mobile !== '') {
+            if (vnf_regex.test(mobile) == false) {
+                toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
+                $("#txtPhone").focus();
+                return false;
+
+            } else {
+                return true;
+            }
+        } else {
+            toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            return false;
+        }
+    }
+    function checkPhoneNumber2() {
+        var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        var mobile = $('#txtPhoneSub').val();
+        if (mobile !== '') {
+            if (vnf_regex.test(mobile) == false) {
+                toastr.error('Số điện thoại của bạn không đúng định dạng.', { timeOut: 5000 })
+                $("#txtPhoneSub").focus();
+                return false;
+
+            } else {
+                return true;
+            }
+        } else {
+            toastr.error('Bạn chưa điền số điện thoại.', { timeOut: 5000 })
+            return false;
+        }
+    }
+});
